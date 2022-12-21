b8532f2ebb300e992f19fa90fc16e754844a2a2a
Mahmoud Saad <saad.mahmoud@protonmail.com>
2022-12-21 22:25:08 +0100

    some minor changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-/* test("renders learn react link", () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-}); */
+});
 
-test("render p element", () => {
+test("renders learn react link", () => {
   render(<App />);
-
-  screen.debug();
-
-  expect(
-    screen.getByText("I was changed on the feature branch")
-  ).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
