6852ee395973933a2775abdda29f30c944a64221
Mahmoud Saad <saad.mahmoud@protonmail.com>
2022-12-21 21:39:17 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>I was changed!</p>
+        <p>I was changed on the feature branch</p>
       </header>
     </div>
   );
